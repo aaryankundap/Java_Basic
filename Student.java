890c94037f8c2205ee53ec63e913a63bc0dd0e76
aaryankundap <165361545+aaryankundap@users.noreply.github.com>
2025-07-17 08:17:29 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,36 @@
+public class Student {
+    // Private fields
+    private String name;
+    private int rollNumber;
+    private char grade;
+
+    // Public getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Public setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Public getter for rollNumber
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    // Public setter for rollNumber
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    // Public getter for grade
+    public char getGrade() {
+        return grade;
+    }
+
+    // Public setter for grade
+    public void setGrade(char grade) {
+        this.grade = grade;
+    }
+}
